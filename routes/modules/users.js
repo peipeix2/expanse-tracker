40e579addcd628008a2ccdeff216b2276b8d6cf5
peipeix2 <peipei.hsueh@outlook.com>
2023-07-23 13:29:26 +0800

    feat: add messae partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -19,11 +19,28 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({message: '所有欄位都是必填'})
+  }
+  if (password !== confirmPassword) {
+    errors.push({message: '密碼與確認密碼不相符'})
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   Users.findOne({email})
     .then((user) => {
       if(user) {
-        console.log('This user already exists.')
+        errors.push({message: '這個Email已經註冊過了'})
         return res.render('register', {
+          errors,
           name,
           email,
           password,
@@ -45,6 +62,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
